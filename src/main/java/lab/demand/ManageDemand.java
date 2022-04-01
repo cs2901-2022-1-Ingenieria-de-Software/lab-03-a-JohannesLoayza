557ac3a7c9767f8b0ef816620638ebdea8ce40d0
AlbertDiLoayz <johannes.loayza.h@utec.edu.pe>
2022-03-31 21:43:25 -0500

    commit ManageDemand.java

--- src/main/java/lab/demand/ManageDemand.java
+++ src/main/java/lab/demand/ManageDemand.java
@@ -1,5 +1,6 @@
 package lab.demand;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ManageDemand {
@@ -10,46 +11,42 @@ public class ManageDemand {
         this.tax = tax;
     }
 
-    public double calculateTotal(List<Order> orders){
-        // Calculate Taxes
+    public double calculateTaxes(List<Order> orders) {
         double taxes = 0.0;
         for (Order order : orders) {
             double tax = this.tax.calculateTax(order.getCountry());
             taxes += tax;
         }
+        return taxes;
+    }
 
-        // Calculate Total
+    public double calculateQuantity(List<Order> orders) {
         double quantities = 0.0;
         for (Order order : orders) {
             double temp = order.getQuantity();
             quantities += temp;
         }
-
-        return quantities * taxes;
+        return quantities;
     }
 
-    public double calculateTotalForWithAdditionalByCountry(List<Order> orders, double defaultAdditionalColombia, double defaultAdditionalPeru, double defaultAdditionalBrazil){
+    public double calculateTotal(List<Order> orders){
+        return calculateTaxes(orders) * calculateQuantity(orders);
+    }
+    
+    public double calculateTotalForWithAdditionalByCountry(List<Order> orders, ArrayList<Double> additional){
         // Calculate additionals by country
         double taxes = 0.0;
         for (Order order : orders) {
             String currCountry = order.getCountry();
             if (currCountry.equals("PE")) {
-                taxes += defaultAdditionalPeru;
+                taxes += additional.get(0);
             } else if (currCountry.equals("BR")) {
-                taxes += defaultAdditionalBrazil;
+                taxes += additional.get(1);
             } else {
-                taxes += defaultAdditionalColombia;
+                taxes += additional.get(2);
             }
         }
-
-        // Calculate Total
-        double quantities = 0.0;
-        for (Order order : orders) {
-            double temp = order.getQuantity();
-            quantities += temp;
-        }
-
-        return quantities * taxes;
+        return calculateQuantity(orders) * taxes;
     }
 
 }
